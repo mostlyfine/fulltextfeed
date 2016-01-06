da5ff83c2af914ea8eaa841ba5f00548736f2ffc
Seiji Sawayanagi <sawayanagi@1101.com>
2016-01-06 22:38:01 +0900

    Add redis cache

--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
 require 'sinatra'
 require 'rss'
 require 'readability_parser'
+require 'redis'
+
+before do
+  @redis = if ENV['REDISTOGO_URL']
+    uri = URI.parse(ENV['REDISTOGO_URL'])
+    Redis.new(host: uri.host, port: uri.port, password: uri.password)
+  else
+    Redis.new(host: '127.0.0.1', port: 6379)
+  end
+end
 
 configure do
   ReadabilityParser.api_token = ENV['API_TOKEN']
@@ -10,9 +20,13 @@ get '/' do
   rss = RSS::Parser.parse(params[:url])
   rss.items.each do |entry|
     begin
-      article = ReadabilityParser.parse(entry.link)
-      entry.description = "<![CDATA[ " + article.content + " ]]>"
-      entry.content_encoded = "<![CDATA[ " + article.content + " ]]>"
+      content = @redis.get(entry.link)
+      content ||= ReadabilityParser.parse(entry.link).content
+      entry.description = "<![CDATA[ " + content + " ]]>"
+      entry.content_encoded = "<![CDATA[ " + content + " ]]>"
+      @redis.set(entry.link, content)
+    rescue => ex
+      logger.error entry.link
     rescue
     end
   end
